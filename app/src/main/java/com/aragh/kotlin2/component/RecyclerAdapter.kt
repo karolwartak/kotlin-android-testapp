c8d9252893ce7c9891f38940b69dc9507f299e8d
Karol Wartak <karol.wartak@miquido.com>
2018-03-29 12:06:50 +0200

    actors

--- app/src/main/java/com/aragh/kotlin2/component/RecyclerAdapter.kt
+++ app/src/main/java/com/aragh/kotlin2/component/RecyclerAdapter.kt
@@ -1,5 +1,7 @@
 package com.aragh.kotlin2.component
 
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.View
 
@@ -9,12 +11,10 @@ abstract class ViewHolder<Item>(view: View) : RecyclerView.ViewHolder(view) {
 }
 
 abstract class Adapter<Item>(
-    private val items: MutableList<Item>,
+    diffCallback: DiffUtil.ItemCallback<Item>,
     private val clickListener: (Item) -> Unit
-) : RecyclerView.Adapter<ViewHolder<Item>>() {
-
-  override fun getItemCount() = items.size
+) : ListAdapter<Item, ViewHolder<Item>>(diffCallback) {
 
   override fun onBindViewHolder(holder: ViewHolder<Item>, position: Int) =
-      holder.bind(items[position], clickListener)
+      holder.bind(getItem(position), clickListener)
 }
--- app/src/main/java/com/aragh/kotlin2/screen/albums/AlbumsActivity.kt
+++ app/src/main/java/com/aragh/kotlin2/screen/albums/AlbumsActivity.kt
@@ -4,20 +4,21 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.util.DiffUtil
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.view.ViewGroup
 import com.aragh.kotlin2.R
-import com.aragh.kotlin2.api.AlbumsRepo
+import com.aragh.kotlin2.actors.Albums
+import com.aragh.kotlin2.actors.GetUserAlbums
 import com.aragh.kotlin2.component.Adapter
 import com.aragh.kotlin2.component.ViewHolder
 import com.aragh.kotlin2.data.Album
 import com.aragh.kotlin2.extensions.inflate
 import kotlinx.android.synthetic.main.activity_albums.*
 import kotlinx.android.synthetic.main.element_album.view.*
+import kotlinx.coroutines.experimental.CompletableDeferred
 import kotlinx.coroutines.experimental.Unconfined
-import kotlinx.coroutines.experimental.channels.consumeEach
-import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import org.koin.android.ext.android.inject
 
@@ -35,10 +36,9 @@ class AlbumsActivity : AppCompatActivity() {
   }
 
 
-  private val albumsRepo: AlbumsRepo by inject()
-  private val albums: MutableList<Album> = mutableListOf()
+  private val albums: Albums by inject()
   private val clickListener: (Album) -> Unit = { }
-  private val adapter = AlbumsAdapter(albums, clickListener)
+  private val adapter = AlbumsAdapter(clickListener)
 
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,20 +52,16 @@ class AlbumsActivity : AppCompatActivity() {
   override fun onStart() {
     super.onStart()
     launch(Unconfined) {
-      val channel = albumsRepo.getUserAlbumsChannel(intent.getIntExtra(USER_ID_EXTRA, 0))
-      channel.consumeEach {
-        albums.add(it)
-        delay(100)
-        runOnUiThread { adapter.notifyItemInserted(adapter.itemCount) }
-      }
+      val albumsDeferred = CompletableDeferred<List<Album>>()
+      albums.userAlbumsActor.offer(GetUserAlbums(intent.getIntExtra(USER_ID_EXTRA, 0), albumsDeferred))
+      val albums = albumsDeferred.await()
+      runOnUiThread { adapter.submitList(albums) }
     }
   }
 
   override fun onStop() {
     super.onStop()
-    val size = albums.size
-    albums.clear()
-    adapter.notifyItemRangeRemoved(0, size)
+    adapter.submitList(null)
   }
 }
 
@@ -79,7 +75,12 @@ class AlbumViewHolder(userView: View) : ViewHolder<Album>(userView) {
   }
 }
 
-class AlbumsAdapter(albums: MutableList<Album>, clickListener: (Album) -> Unit) : Adapter<Album>(albums, clickListener) {
+class AlbumsAdapter(clickListener: (Album) -> Unit) : Adapter<Album>(AlbumDiffCallback(), clickListener) {
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
       AlbumViewHolder(parent.inflate(R.layout.element_album))
+}
+
+class AlbumDiffCallback : DiffUtil.ItemCallback<Album>() {
+  override fun areItemsTheSame(oldItem: Album?, newItem: Album?) = oldItem?.id == newItem?.id
+  override fun areContentsTheSame(oldItem: Album?, newItem: Album?) = oldItem == newItem
 }
