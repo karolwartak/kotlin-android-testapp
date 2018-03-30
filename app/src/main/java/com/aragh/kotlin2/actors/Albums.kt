4433b52ed68167186c99fa14dbea38d9ebcf2d11
Karol Wartak <karol.wartak@miquido.com>
2018-03-30 15:14:47 +0200

    posting

--- app/src/main/java/com/aragh/kotlin2/actors/Albums.kt
+++ app/src/main/java/com/aragh/kotlin2/actors/Albums.kt
@@ -2,6 +2,7 @@ package com.aragh.kotlin2.actors
 
 import com.aragh.kotlin2.api.AlbumsApi
 import com.aragh.kotlin2.data.Album
+import com.aragh.kotlin2.data.NewAlbum
 import kotlinx.coroutines.experimental.CompletableDeferred
 import kotlinx.coroutines.experimental.channels.actor
 
@@ -11,6 +12,11 @@ class Albums(private val albumsApi: AlbumsApi) {
     for (msg in channel) {
       when (msg) {
         is GetUserAlbums -> msg.response.complete(albumsApi.userAlbums(msg.userId).execute().body() ?: emptyList())
+        is PostUserAlbum -> {
+          val newAlbumLocation = albumsApi.postAlbum(msg.userId, NewAlbum(msg.title)).execute().headers()["location"]
+          val id = newAlbumLocation?.run { substring(lastIndexOf('/') + 1).toInt() }
+          msg.response.complete(Album(id!!, msg.title))
+        }
       }
     }
   }
@@ -19,3 +25,4 @@ class Albums(private val albumsApi: AlbumsApi) {
 
 sealed class AlbumsMessage
 class GetUserAlbums(val userId: Int, val response: CompletableDeferred<List<Album>>) : AlbumsMessage()
+class PostUserAlbum(val userId: Int, val title: String, val response: CompletableDeferred<Album>) : AlbumsMessage()
--- app/src/main/java/com/aragh/kotlin2/api/Apis.kt
+++ app/src/main/java/com/aragh/kotlin2/api/Apis.kt
@@ -1,9 +1,12 @@
 package com.aragh.kotlin2.api
 
 import com.aragh.kotlin2.data.Album
+import com.aragh.kotlin2.data.NewAlbum
 import com.aragh.kotlin2.data.User
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 
@@ -12,7 +15,11 @@ interface UsersApi {
   fun users() : Call<List<User>>
 }
 
+
 interface AlbumsApi {
   @GET("/users/{id}/albums")
   fun userAlbums(@Path("id") userId: Int) : Call<List<Album>>
+
+  @POST("/users/{id}/albums")
+  fun postAlbum(@Path("id") userId: Int, @Body album: NewAlbum) : Call<Unit>
 }
